b745ac1eefbc318ad7499d3cba588423b6d52ad8
gyeongmin-03 <lkm1282@naver.com>
2024-03-24 16:14:04 +0900

    이미 5(1.3)이 있어서 6(1.4)로 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.akj.lmswidget"
         minSdk = 31
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.3"
+        versionCode = 6
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
